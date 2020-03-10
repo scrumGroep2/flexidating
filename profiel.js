c18b0d776b2a76bed58c53d9583c120b0f726d90
stephane jouk <stephane.jouk@vdab.be>
2020-03-10 14:22:00 +0100

    fetch werkende

--- profiel.js
+++ profiel.js
@@ -4,7 +4,14 @@ console.log(id)
 if (id===null){
     document.getElementById("geenLogin").innerText="gelieven in te loggen"
 }else{
-    async function leesUser(){
-        const response = await fetch()
-    }
+   leesUser();
+}
+async function leesUser(){
+fetch(`https://scrumserver.tenobe.org/scrum/api/profiel/read_one.php?id=${id}`)
+                    .then(function (resp)   { return resp.json(); })
+                    .then(function (data)   { 
+                        console.log(data); 
+                        document.getElementById("username").innerText=data.nickname;
+                     })
+                    .catch(function (error) { console.log(error); });
 }
