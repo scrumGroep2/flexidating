9a4e441bb50ffc341e07b0ffe0db59b25ea7bda5
Sandra Geerinckx <sandra.geerinckx@telenet.be>
2020-03-10 14:09:46 +0100

    button verwijderen

--- profiel.js
+++ profiel.js
@@ -8,3 +8,24 @@ if (id===null){
         const response = await fetch()
     }
 }
+
+document.getElementById("verwijderen").onclick = function () {
+    const foutVerwerkenGegevens=document.getElementById("foutVerwerkenGegevens");
+    foutVerwerkenGegevens.style.display = "";
+    let url="https://scrumserver.tenobe.org/scrum/api/profiel/delete.php";
+    let data = {id: id}
+
+    var request = new Request(url, {
+                method: 'DELETE',
+                body: JSON.stringify(data),
+                headers: new Headers({'Content-Type': 'application/json'})
+                });
+
+    let response = fetch(request);
+    if (response.ok) {
+        sessionStorage.removeItem("id");
+        window.location.href = "home.html";
+    } else {
+        foutVerwerkenGegevens.style.display = "inline";
+    }
+}
